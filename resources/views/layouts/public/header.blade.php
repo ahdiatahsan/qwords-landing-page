eb76dc20aee7a8c7931c0a3ea556cd4ee9396053
ahdiatahsan <ahdiatahsanaa@gmail.com>
2023-05-09 00:10:29 +0800

    add(layouts/public): header blade

---
+++ resources/views/layouts/public/header.blade.php
@@ -0,0 +1,13 @@
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Qwords.com: Cloud Web Hosting Indonesia Akses Cepat Aman</title>
+  
+    <!--begin::Global Theme Styles(used by all pages)-->
+    <link rel="shortcut icon" href="{{ asset('media/logos/favicon.png') }}" type="image/x-icon">
+    <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
+    @vite('resources/css/app.css')
+    <!--end::Global Theme Styles-->
+</head>
